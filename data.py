db7032beb1ca5daaf5f80138ebe40d7aa80918ee
shay solomon <shays433@gmail.com>
2025-04-22 22:54:24 +0300

    added more pokemons and changed logic of game

--- data.py
+++ data.py
@@ -4,11 +4,17 @@ image_folder = "images/front_images"
 image_files = os.listdir(image_folder)
 
 class cardPokemon:
-        def __init__(self, id, image_front, image_back = "images/pokemon_card_backside.png", orientation = "up"):
-                self.id = id
-                self.image_front = image_front
-                self.image_back = image_back
-                self.orientation = orientation
+    def __init__(self, id, image_front, image_back = "images/pokemon_card_backside.png", orientation = "face_up"):
+            self.id = id
+            self.image_front = image_front
+            self.image_back = image_back
+            self.orientation = orientation
+
+    def change_orientation():
+        if cardPokemon.change_orientation == "face_up":
+            cardPokemon.orientation = "face_down" 
+        else:
+            cardPokemon.change_orientation = "face_up"
 
 cardDeck = []
 for index, filename in enumerate (image_files):        
--- screen.py
+++ screen.py
@@ -1,38 +1,59 @@
-import tkinter
+from tkinter import ttk
 from tkinter import *
+import tkinter.messagebox
 from PIL import Image, ImageTk  
 from data import *
- 
-
-# variables
-base_color = "#008000"
 
 # Create main window
 root = Tk()
 root.title("My First Memory Game")
+
+
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-
-
+#color
+base_color = "#008000"
 root.geometry(f"{screen_width}x{screen_height}")
-root.configure(bg=base_color)
+
+
+style= ttk.Style()
+style.configure("Green.TFrame", background=base_color)
+mainframe = ttk.Frame(root, padding="3 3 12 12", style="Green.TFrame")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
 #Card variables
-width = 150
-height = 225
+width = int(screen_height/11*2)
+height = int(screen_height/11*3)
+padding_width = int((screen_width - width * 8 ) / 18)
 
-#iterate and print all the card states
-for card in cardDeck:
-    if card.orientation == "up":
-        image = Image.open(card.image_front)
-    else:
-        image = Image.open(card.image_back)
-    image = image.resize((width, height))
-    photo = ImageTk.PhotoImage(image)
-    label = tkinter.Label(image=photo, bg = base_color)
-    label.image = photo
-    label.pack(pady=5)
+#number of cards for screen 
 
 
-# Run the GUI loop
-root.mainloop()
+def message_to_user(message:str):
+    tkinter.messagebox.showinfo("Info",message)
+
+
+def activate_screen():
+    # variables
+    #iterate and print all the card states
+    for i, card in enumerate(cardDeck):
+        if card.orientation == "face_up":
+            image = Image.open(card.image_front)
+        else:
+            image = Image.open(card.image_back)
+        image = image.resize((width, height))
+        photo = ImageTk.PhotoImage(image)
+        label = tkinter.Label(mainframe, image=photo, bg = base_color)
+        label.image = photo
+
+        label.grid(row=i // 8, column=i % 8, padx=padding_width, pady=10)
+
+
+    # Run the GUI loop
+    root.mainloop()
+
