712bb49fdb1a205abbc5fb2638ba071eb6707172
shay solomon <shays433@gmail.com>
2025-04-24 02:36:36 +0300

    changed logic, added functionality of screen and flipping of cards

--- data.py
+++ data.py
@@ -1,24 +1,41 @@
 import os
+import random
+from PIL import Image, ImageTk  
 
 image_folder = "images/front_images"
 image_files = os.listdir(image_folder)
 
 class cardPokemon:
-    def __init__(self, id, image_front, image_back = "images/pokemon_card_backside.png", orientation = "face_up"):
+    def __init__(self, id, image_front, image_back = "images/pokemon_card_backside.png", orientation = "face_down"):
             self.id = id
             self.image_front = image_front
             self.image_back = image_back
             self.orientation = orientation
 
-    def change_orientation():
-        if cardPokemon.change_orientation == "face_up":
-            cardPokemon.orientation = "face_down" 
+    def change_orientation(self):
+        if self.orientation == "face_up":
+            self.orientation = "face_down" 
         else:
-            cardPokemon.change_orientation = "face_up"
+            self.orientation = "face_up"
+    
+
+
 
 cardDeck = []
+
 for index, filename in enumerate (image_files):        
         image_path = os.path.join(image_folder , filename)
         cardDeck.append (cardPokemon(index + 1, image_path))
 
+def randomize_deck(cardDeck):
+    random.shuffle(cardDeck)
 
+def fixed_pokemon_deck(cardDeck, height, width):
+    number_cards = height * width / 2
+    number_cards = int(number_cards)
+    cardDeck = cardDeck[0:number_cards]
+    cardDeck = cardDeck * 2
+    randomize_deck(cardDeck)
+    return cardDeck
+
+     
